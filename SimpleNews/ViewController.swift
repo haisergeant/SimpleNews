e636f22891d8f8b9d0d7b52731e7abb503dd54d6
Hai Le Thanh <haisergeant@gmail.com>
2021-02-18 06:32:31 +1100

    Initial Commit

---
+++ SimpleNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//
+//  Created by Hai Le Thanh.
+//  
+//
+	
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
